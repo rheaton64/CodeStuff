47bd1e13931fbb6e45da191b7a9bcf4e3e76757f
Ryan Heaton <rheaton@10.144.5.44>
2018-06-24 16:28:15 -0400

    Pretty much finished Minesweeper

--- Ryan/src/Minesweeper/Board.java
+++ Ryan/src/Minesweeper/Board.java
@@ -3,41 +3,70 @@ package Minesweeper;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.Random;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 //Ryan Heaton
+//Minesweeper
 //Board class made up of cells that runs the game
 
 public class Board extends JFrame{
+
+	//Boolean to reset game
+	private static boolean isReset;
+
 	//Main method to run the program
 	public static void main(String[] args) {
-		Board board = new Board(9);
+		isReset = false;
+		while(true){
+			isReset = false;
+			//Creates the board (size, number of mines)
+			Board board = new Board(9, 10);
+			while(!isReset){
+
+			}
+		}
 	}
-	
+
 	//Instance variables
+
+	//2D-Array to store grid of cells
 	private MineCell[][] panel;
+	//Length of one side of the grid, should be 9 until changes are made to images
 	private int size;
-	
-	public Board(int size){
+	//Number of mines in the grid, recommened 10 until above changes are made, can't be more than size^2
+	private int numMines;
+	//Counter for flags on the board
+	private int flagCounter;
+
+	//Constructor for the board
+	public Board(int size, int numMines){
 		//Initialize instance variables
 		this.size = size;
 		panel = new MineCell[size][size];
-		
+		this.numMines = numMines;
+		flagCounter = 0;
+
 		//Initialize Frame
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setBounds(0,0,603,603);
-		
-		//Create GridLayout
+		setBounds(0,0,603,603+154);
+		setResizable(false);
+
+		//Create grid panel
 		JPanel grid = new JPanel();
 		grid.setLayout(new GridLayout(size,size));
-		
+		grid.setBounds(0, 134, 603, 603);
+
 		//Fill panel array with cells
 		for(int r = 0;r < size;r++){
 			for(int c = 0;c < size;c++){ 
@@ -45,35 +74,124 @@ public class Board extends JFrame{
 				panel[r][c].repaint();
 			}
 		}
-		
+
 		//Fill grid with cells
-		for(int x = 0;x < size;x++){
-			for(int y = 0;y < size;y++){
-				grid.add(panel[x][y]);
+		for(int r = 0;r < size;r++){
+			for(int c = 0;c < size;c++){
+				grid.add(panel[r][c]);
 			}
 		}
-		
+
+		//Add menu panel
+		JPanel menu = new JPanel();
+		menu.setBackground(Color.gray);
+		menu.setBounds(0, 0, 603, 67);
+		menu.setLayout(null);
+
+		//Label for remaining mines
+		JLabel mines = new JLabel("Mines Left: "+(numMines-flagCounter));
+		mines.setFont(new Font("Comic Sans", 0, 25));
+		mines.setBounds(0, 105, 200, 20);
+		menu.add(mines);
+
+		//Button to reset game
+		JButton reset = new JButton();
+		reset.setBounds(268, 0, 67, 67);
+		reset.setBackground(Color.white);
+		reset.setText("Reset");
+		reset.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				//Resets and reassigns all the cells
+				for(int r = 0;r < size;r++){
+					for(int c = 0;c < size;c++){
+						panel[r][c].reset();
+					}
+				}
+				setMines(numMines);
+				for(int r = 0;r < size;r++){
+					for(int c = 0;c < size;c++){
+						panel[r][c].setLabel(getAdjMines(r,c), r, c);
+					}
+				}
+			}
+		});
+
+		menu.add(reset);
+
+		//Adds panels to the frame
 		add(grid);
-		setMines(10);
+		add(menu);
+
+		//Generates mines and gives each cell a label
+		setMines(this.numMines);
 		for(int r = 0;r < size;r++){
 			for(int c = 0;c < size;c++){
 				panel[r][c].setLabel(getAdjMines(r,c), r, c);
 			}
 		}
-		repaint();
+
+		//Game loop to test for certain events
 		while(true){
+			//Boolean if clearing cells should start
+			boolean clearing = true;
 			System.out.print("");
-			if(!MineCell.clear.equals("")){
+			//Clears groups of cells
+			if(!MineCell.clear.equals("") && clearing){
 				System.out.println("hi");
 				int r = Integer.parseInt(MineCell.clear.substring(0,1));
 				int c = Integer.parseInt(MineCell.clear.substring(1));
 				MineCell.clear = "";
 				clearTiles(r,c);
+				for(int nR = 0;nR < size;nR++){
+					for(int nC = 0;nC < size;nC++){
+						panel[nR][nC].hasBeenCleared = false;
+						//panel[nR][nC].fixCleared();
+					}
+				}
+				clearing = false;
+				MineCell.clear = "";
 			}	
+
+			//Win condition
+			if(MineCell.spacesCleared == size*size-this.numMines){
+				System.out.println("You did it");
+				MineCell.spacesCleared++;
+			}
+
+			//Loss Condition
+			if(MineCell.lost){
+				for(int r = 0;r < size;r++){
+					for(int c = 0;c < size;c++){
+						if(panel[r][c].isMine()){
+							panel[r][c].setImage();
+						}
+					}
+				}
+				System.out.println("You lost!");
+				MineCell.lost = false;
+			}
+
+			//Keeps track of the number of flags and displays remaining mines
+			if(MineCell.numFlags != flagCounter){
+				flagCounter = MineCell.numFlags;
+				mines.setText("Mines Left: "+(numMines-flagCounter));
+			}
+			
+			//Counts number of spaces cleared
+			if(MineCell.shouldCount){
+				MineCell.spacesCleared = 0;
+				for(int r = 0;r < size;r++){
+					for(int c = 0;c < size;c++){
+							if(panel[r][c].isClear())
+								MineCell.spacesCleared++;
+					}
+				}
+				System.out.println("Spaces Cleared: "+MineCell.spacesCleared);
+				MineCell.shouldCount = false;
+			}
 		}
-		
 	}
-	
+
 	//Method to set the mines
 	//num = number of mines
 	public void setMines(int num){
@@ -92,246 +210,74 @@ public class Board extends JFrame{
 			set = false;
 		}
 	}
+
+	//Resets the game
+	public void resetGame(){
+
+	}
+
 	//Gets number of adjacent mines, could probably be improved
+	//r = row of cell, c = column of cell
 	public int getAdjMines(int r, int c){
+		int testR = 0;
+		int testC = 0;
+		boolean check = true;
 		int mines = 0;
-		if(r == 0){
-			if(c == 0){
-				if(panel[0][1].isMine())
-					mines++;
-				if(panel[1][0].isMine())
-					mines++;
-				if(panel[1][1].isMine())
+		for(int i = 0; i < 8; i++){
+			check = true;
+			switch(i){
+			case 0: testR=r+1;testC=c+1;break;
+			case 1: testR=r;testC=c+1;break;
+			case 2: testR=r-1;testC=c+1;break;
+			case 3: testR=r-1;testC=c;break;
+			case 4: testR=r-1;testC=c-1;break;
+			case 5: testR=r;testC=c-1;break;
+			case 6: testR=r+1;testC=c-1;break;
+			case 7: testR=r+1;testC=c;break;
+			}
+			if(testR < 0 || testR > size-1 || testC < 0 || testC > size-1){
+				check = false;
+			}
+			if(check){
+				if(panel[testR][testC].isMine())
 					mines++;
 			}
-			else if(c == size-1){
-				if(panel[0][size-2].isMine())
-					mines++;
-				if(panel[1][size-2].isMine())
-					mines++;
-				if(panel[1][size-1].isMine())
-					mines++;
-			}
-			else{
-				if(panel[0][c-1].isMine())
-					mines++;
-				if(panel[1][c-1].isMine())
-					mines++;
-				if(panel[1][c].isMine())
-					mines++;
-				if(panel[1][c+1].isMine())
-					mines++;
-				if(panel[0][c+1].isMine())
-					mines++;
-			}
-		}
-		else if(c == 0){
-			if(r == size-1){
-				if(panel[size-2][0].isMine())
-					mines++;
-				if(panel[size-2][1].isMine())
-					mines++;
-				if(panel[size-1][1].isMine())
-					mines++;
-			}
-			else{
-				if(panel[r-1][0].isMine())
-					mines++;
-				if(panel[r-1][1].isMine())
-					mines++;
-				if(panel[r][1].isMine())
-					mines++;
-				if(panel[r+1][1].isMine())
-					mines++;
-				if(panel[r+1][0].isMine())
-					mines++;
-			}
-		}
-		else if(r == size-1){
-			if(c == size-1){
-				if(panel[size-2][size-1].isMine())
-					mines++;
-				if(panel[size-2][size-2].isMine())
-					mines++;
-				if(panel[size-1][size-2].isMine())
-					mines++;
-			}
-			else{
-				if(panel[size-1][c-1].isMine())
-					mines++;
-				if(panel[size-2][c-1].isMine())
-					mines++;
-				if(panel[size-2][c].isMine())
-					mines++;
-				if(panel[size-2][c+1].isMine())
-					mines++;
-				if(panel[size-1][c+1].isMine())
-					mines++;
-			}
-		}
-		else if(c == size-1){
-			if(panel[r-1][size-1].isMine())
-				mines++;
-			if(panel[r-1][size-2].isMine())
-				mines++;
-			if(panel[r][size-2].isMine())
-				mines++;
-			if(panel[r+1][size-2].isMine())
-				mines++;
-			if(panel[r+1][size-1].isMine())
-				mines++;
-		}
-		else{
-			if(panel[r-1][c-1].isMine())
-				mines++;
-			if(panel[r-1][c].isMine())
-				mines++;
-			if(panel[r-1][c+1].isMine())
-				mines++;
-			if(panel[r][c-1].isMine())
-				mines++;
-			if(panel[r][c+1].isMine())
-				mines++;
-			if(panel[r+1][c-1].isMine())
-				mines++;
-			if(panel[r+1][c].isMine())
-				mines++;
-			if(panel[r+1][c+1].isMine())
-				mines++;
 		}
 		return mines;
 	}
 	
+	//Clears groups of empty cells
+	//r = row of cell, c = column of cell
 	public void clearTiles(int r, int c){
-		if(r == 0){
-			if(c == 0){
-				if(panel[0][1].getLabel() == 0){
-					System.out.print("1");
-					panel[0][1].setImage();
-					clearTiles(0, 1);
-				}if(panel[1][0].getLabel() == 0){
-					System.out.print("2");
-					panel[1][0].setImage();
-					clearTiles(1, 0);
-				}if(panel[1][1].getLabel() == 0){
-					System.out.print("3");
-					panel[1][1].setImage();
-					clearTiles(1, 1);
-				}}
-			else if(c == size-1){
-				if(panel[0][size-2].getLabel() == 0){
-					System.out.print("found");
-					panel[0][size-2].setImage();
-					clearTiles(0, size-2);
-				}	if(panel[1][size-2].getLabel() == 0){
-					System.out.print("found");
-					panel[1][size-2].setImage();
-					clearTiles(1, size-2);
-				}	if(panel[1][size-1].getLabel() == 0){
-					System.out.print("found");
-					panel[1][size-1].setImage();
-					clearTiles(1, size-1);
-				}}
-			else{
-				if(panel[0][c-1].getLabel() == 0){
-					System.out.print("found");
-					panel[0][c-1].setImage();
-					clearTiles(0, c-1);
-				}if(panel[1][c-1].getLabel() == 0){
-					System.out.print("found");
-					panel[1][c-1].setImage();
-					clearTiles(1, c-1);
-				}if(panel[1][c].getLabel() == 0){
-					System.out.println("found");
-					panel[1][c].setImage();
-					clearTiles(1, c);
-				}	if(panel[1][c+1].getLabel() == 0){
-					System.out.print("found");
-					panel[1][c+1].setImage();
-					clearTiles(1, c+1);
-				}if(panel[0][c+1].getLabel() == 0){
-					System.out.print("found");
-					panel[1][c-1].setImage();
-					clearTiles(0, c+1);
-			}}
-		}
-		else if(c == 0){
-			if(r == size-1){
-				if(panel[size-2][0].getLabel() == 0)
-					System.out.print("found");
-				if(panel[size-2][1].getLabel() == 0)
-					System.out.print("found");
-				if(panel[size-1][1].getLabel() == 0)
-					System.out.print("found");
+		panel[r][c].hasBeenCleared = true;
+		int testR = 0;
+		int testC = 0;
+		boolean check = true;
+		int mines = 0;
+		for(int i = 0; i < 8; i++){
+			check = true;
+			switch(i){
+			case 0: testR=r+1;testC=c+1;break;
+			case 1: testR=r;testC=c+1;break;
+			case 2: testR=r-1;testC=c+1;break;
+			case 3: testR=r-1;testC=c;break;
+			case 4: testR=r-1;testC=c-1;break;
+			case 5: testR=r;testC=c-1;break;
+			case 6: testR=r+1;testC=c-1;break;
+			case 7: testR=r+1;testC=c;break;
 			}
-			else{
-				if(panel[r-1][0].getLabel() == 0)
-					System.out.print("found");
-				if(panel[r-1][1].getLabel() == 0)
-					System.out.print("found");
-				if(panel[r][1].getLabel() == 0)
-					System.out.print("found");
-				if(panel[r+1][1].getLabel() == 0)
-					System.out.print("found");
-				if(panel[r+1][0].getLabel() == 0)
-					System.out.print("found");
+			if(testR < 0 || testR > size-1 || testC < 0 || testC > size-1){
+				check = false;
+			}
+			if(check){
+				if(!panel[testR][testC].hasBeenCleared){
+					panel[testR][testC].setImage();
+					if(panel[testR][testC].getLabel() == 0){
+						panel[testR][testC].hasBeenCleared = true;
+						clearTiles(testR, testC);
+					}
+				}
 			}
 		}
-		else if(r == size-1){
-			if(c == size-1){
-				if(panel[size-2][size-1].getLabel() == 0)
-					System.out.print("found");
-				if(panel[size-2][size-2].getLabel() == 0)
-					System.out.print("found");
-				if(panel[size-1][size-2].getLabel() == 0)
-					System.out.print("found");
-			}
-			else{
-				if(panel[size-1][c-1].getLabel() == 0)
-					System.out.print("found");
-				if(panel[size-2][c-1].getLabel() == 0)
-					System.out.print("found");
-				if(panel[size-2][c].getLabel() == 0)
-					System.out.print("found");
-				if(panel[size-2][c+1].getLabel() == 0)
-					System.out.print("found");
-				if(panel[size-1][c+1].getLabel() == 0)
-					System.out.print("found");
-			}
-		}
-		else if(c == size-1){
-			if(panel[r-1][size-1].getLabel() == 0)
-				System.out.print("found");
-			if(panel[r-1][size-2].getLabel() == 0)
-				System.out.print("found");
-			if(panel[r][size-2].getLabel() == 0)
-				System.out.print("found");
-			if(panel[r+1][size-2].getLabel() == 0)
-				System.out.print("found");
-			if(panel[r+1][size-1].getLabel() == 0)
-				System.out.print("found");
-		}
-		else{
-			if(panel[r-1][c-1].getLabel() == 0)
-				System.out.print("found");
-			if(panel[r-1][c].getLabel() == 0)
-				System.out.print("found");
-			if(panel[r-1][c+1].getLabel() == 0)
-				System.out.print("found");
-			if(panel[r][c-1].getLabel() == 0)
-				System.out.print("found");
-			if(panel[r][c+1].getLabel() == 0)
-				System.out.print("found");
-			if(panel[r+1][c-1].getLabel() == 0)
-				System.out.print("found");
-			if(panel[r+1][c].getLabel() == 0)
-				System.out.print("found");
-			if(panel[r+1][c+1].getLabel() == 0)
-				System.out.print("found");
-		}
-	}
-	
-	public static void clear(){
-		
 	}
 }
--- Ryan/src/Minesweeper/MineCell.java
+++ Ryan/src/Minesweeper/MineCell.java
@@ -15,72 +15,143 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 //Ryan Heaton
+//Minesweeper
 //Cell class that makes up grid on the Board
 
 
 public class MineCell extends JPanel implements MouseListener{
 	//Instace variables
+
+	//length of board in cells, parameter of constructor
 	private int boardSize;
+	//Size of each cell in pixels
 	private int cellSize;
-	private Color color;
+	//Is the cell a mine?
 	private boolean isMine;
+	//Image to be shown on cell
 	private Image i;
+	//Is this cell being initialized?
 	private boolean init;
+	//Label of the cell, number of adj mines
 	private int label;
+	//Coordinates of the cell on the board
 	private String coord;
+	//Has this cell been cleared?
+	private boolean isClear;
+	//Has the cell been flagged?
+	private boolean isFlagged;
+	//Has this cell been revealed?
+	private boolean isRevealed;
+	//WIP String for clearing tiles
 	public static String clear = "";
-	
+	//How many spaces have been cleared on the board?
+	public static int spacesCleared;
+	//Did you lose the game?
+	public static boolean lost;
+	//Number of flags on the board
+	public static int numFlags;
+	//Checks if this has been cleared - used during clears
+	public boolean hasBeenCleared;
+	//Should the cleared spaces be counted?
+	public static boolean shouldCount;
+
+	//Constructor for the cell
 	public MineCell(int size){
 		boardSize = size;
-		//Side length of board / Number of 
+		//Pixel length of board / Cell length of board 
 		cellSize = 603/boardSize;
 		isMine = false;
-		color = Color.gray;
 		init = true;
-		label = 3;
+		label = 0;
+		spacesCleared = 0;
+		isClear = false;
+		isFlagged = false;
+		lost = false;
+		isRevealed = false;
+		shouldCount = false;
 		addMouseListener(this);
 	}
-	
-	//Paint method to deal with setting the images
+
+	//Paint method to set the images
 	public void paint(Graphics g){
+		//If the cells are being initiated
 		if(init){
-			setSpaces();
+			setSpace();
 			init = false;
 		}
+		//Draws the correct image
 		g.drawImage(i, 0, 0, null);
 	}
-	
+
+	//Sets this cell as a mine
 	public void setMine(){
 		isMine = true;
 	}
-	
+
+	//Is the cell a mine?
 	public boolean isMine(){
 		return isMine;
 	}
-	
+
+	//Sets the label and coords of the cell
 	public void setLabel(int label, int r, int c){
 		this.label = label;
 		coord = "" + r + c;
 	}
-	
+
+	//Gets the cell's label
 	public int getLabel(){
 		return label;
 	}
 	
-	private void setSpaces(){
+	//Has this cell been counted as clear?
+	public boolean isClear(){
+		return isClear;
+	}
+	
+	//Fixes the count of cleared cells
+	public void fixCleared(){
+		if(isRevealed){
+			spacesCleared++;
+			System.out.println("Spaces cleared: "+spacesCleared);
+			isRevealed = false;
+			isClear = true;
+		}
+	}
+	
+	//Resets the cell
+	public void reset(){
+		isMine = false;
+		init = true;
+		label = 0;
+		spacesCleared = 0;
+		isClear = false;
+		isFlagged = false;
+		lost = false;
+		numFlags = 0;
+		repaint();
+		
+	}
+
+	//Sets this cell as an unrevealed space
+	private void setSpace(){
 		try {
-			if(!isMine)
-				i = ImageIO.read(new File("src/Minesweeper/Blank.png"));
-			else
-				i = ImageIO.read(new File("src/Minesweeper/BlankMine.png"));
+			i = ImageIO.read(new File("src/Minesweeper/Blank.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
+	//Sets the image of the cell based on the label
 	public void setImage(){
 		try {
-				if(!isMine)
+			//Didn't hit a mine
+			if(!isMine){
+				//Hasn't been flagged
+				if(!isFlagged){
+					isClear = true;
+					isRevealed = true;
+					//Reveals number based on label
 					switch(label){
 					case 0: i = ImageIO.read(new File("src/Minesweeper/BlankSpace.png"));
 					clear = coord;
@@ -101,36 +172,57 @@ public class MineCell extends JPanel implements MouseListener{
 					break;
 					case 8: i = ImageIO.read(new File("src/Minesweeper/Eight.png"));
 					break;
-				}
-				//Hit a mine
-				else{
-					i = ImageIO.read(new File("src/Minesweeper/HitMine.png"));
+					}
 				}
 			}
-			catch (IOException e) {
-				e.printStackTrace();
+			//Hit a mine
+			else{
+				i = ImageIO.read(new File("src/Minesweeper/HitMine.png"));
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
 		}
 		repaint();
 	}
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	//Called if you clicked on this cell
 	@Override
 	public void mousePressed(MouseEvent e) {
 		try {
-			//Places flag
-			if(e.isControlDown()){
-				i = ImageIO.read(new File("src/Minesweeper/Flag.png"));
+			//Toggles flag when control-clicked
+			if(e.isControlDown() && !isClear){
+				if(!isFlagged){
+					i = ImageIO.read(new File("src/Minesweeper/Flag.png"));
+					isFlagged = true;
+					numFlags++;
+				}
+				else{
+					i = ImageIO.read(new File("src/Minesweeper/Blank.png"));
+					isFlagged = false;
+					numFlags--;
+				}
 			}
 			else{
-				//Tests for label to create number
-				if(!isMine)
+				//Didn't hit a mine
+				if(!isMine){
+					//Increments spaces cleared if this space is being revealed
+					if(!isClear){
+						//spacesCleared++;
+						//System.out.println("Spaces cleared: "+spacesCleared);
+						isClear = true;
+						//Stars clearing cells
+						if(label == 0)
+							clear = coord;
+					}
+					//Reveals number based on label
 					switch(label){
 					case 0: i = ImageIO.read(new File("src/Minesweeper/BlankSpace.png"));
-					clear = coord;
 					break;
 					case 1: i = ImageIO.read(new File("src/Minesweeper/One.png"));
 					break;
@@ -148,33 +240,36 @@ public class MineCell extends JPanel implements MouseListener{
 					break;
 					case 8: i = ImageIO.read(new File("src/Minesweeper/Eight.png"));
 					break;
-				}
+					}}
 				//Hit a mine
 				else{
 					i = ImageIO.read(new File("src/Minesweeper/HitMine.png"));
+					//Sets the game to lost
+					lost = true;
 				}
 			}
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
 		repaint();
+		shouldCount = true;
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
